2ead639cad8734f05cfb91ecfccbb101c8f465bf
rudy <rodolfo.gamarra@globant.com>
2021-11-02 16:48:58 -0500

    update ip

--- conf.js
+++ conf.js
@@ -10,7 +10,7 @@ exports.config = {
             'browserName': 'MicrosoftEdge',
         }
     ],
-    seleniumAddress: 'http://172.19.0.2:4444/wd/hub',
+    seleniumAddress: 'http://172.17.0.1:4444/wd/hub',
     specs: ['./**/*.spec.js'],
     framework: 'jasmine2',
     onPrepare: function () {
